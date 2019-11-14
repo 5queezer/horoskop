da237f6b41e6628c9a7173581e88d885aac9bbf2
Christian Pojoni <dev@pojoni.at>
2019-11-14 20:53:32 +0100

    wrong zodiac fix

--- horoscopecom.rb
+++ horoscopecom.rb
@@ -1,13 +1,15 @@
 
 def horoscopecom(zodiac)
+  # TODO bugfix index in url
     zodiacs = %w{widder stier zwillinge krebs loewe jungfrau waage skorpion schuetze steinbock wassermann fische}
+    zodiac_map = Hash[zodiacs.zip 1..12]
     zodiacs &= zodiac unless zodiac.first == "all"
     abort("Invalid zodiac '#{zodiac.join(',')}'") if zodiacs.empty?
   
     result = {}
   
-    zodiacs.each_with_index do |zodiac,idx|
-      doc = Nokogiri::HTML(open("https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-today.aspx?sign=#{idx+1}"))
+    zodiacs.each do |zodiac|
+      doc = Nokogiri::HTML(open("https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-today.aspx?sign=#{zodiac_map[zodiac]}"))
       contents = doc.css('.main-horoscope > p:nth-child(2)').children
       date_string = contents.css('strong').text.strip
 
