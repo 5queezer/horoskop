4846fe78bbc756edd9b5840c49e7cc72d851a865
Christian Pojoni <dev@pojoni.at>
2019-11-14 04:00:47 +0100

    comment removed

--- horoskop.rb
+++ horoskop.rb
@@ -7,11 +7,13 @@ require 'pp'
 require 'optimist'
 require 'yaml'
 require 'date'
+require 'google-translate'
 require_relative 'date_german_additions'
 require_relative 'astrowoche'
 require_relative 'astroportal'
 require_relative 'kroneat'
 require_relative 'kurierat'
+require_relative 'horoscopecom'
 
 opts = Optimist::options do
   opt :zodiac, "Choose zodiac", :type => :string, :default => "all"       
@@ -39,6 +41,12 @@ threads << Thread.new {
   contents = astrowoche opts[:zodiac].split(',')
   puts contents.to_yaml
 }
- 
+
+threads << Thread.new {
+  contents = horoscopecom opts[:zodiac].split(',')
+  puts contents.to_yaml
+}
+
 threads.each(&:join) 
+
 
