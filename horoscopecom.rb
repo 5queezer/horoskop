fe76944f5c887608cf9c29ff5abdbebb12d9c9d6
Christian Pojoni <dev@pojoni.at>
2019-11-15 23:59:38 +0100

    remove unnecessary code

--- horoscopecom.rb
+++ horoscopecom.rb
@@ -21,27 +21,3 @@ class HoroscopeCom < Horoscope
     end
   end
 end
-
-def horoscopecom(zodiac)
-  # TODO bugfix index in url
-    zodiacs = %w{widder stier zwillinge krebs loewe jungfrau waage skorpion schuetze steinbock wassermann fische}
-    zodiac_map = Hash[zodiacs.zip 1..12]
-    zodiacs &= zodiac unless zodiac.first == "all"
-    abort("Invalid zodiac '#{zodiac.join(',')}'") if zodiacs.empty?
-  
-    result = {}
-  
-    zodiacs.each do |zodiac|
-      doc = Nokogiri::HTML(open("https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-today.aspx?sign=#{zodiac_map[zodiac]}"))
-      contents = doc.css('.main-horoscope > p:nth-child(2)').children
-      date_string = contents.css('strong').text.strip
-
-      body = contents.text.strip.gsub("#{date_string} - ", '')
-
-      result[zodiac.to_sym] = {
-        :date => Date.parse(date_string),
-        :body => body 
-      }
-    end
-    result
-  end
