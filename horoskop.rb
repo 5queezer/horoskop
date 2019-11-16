024ab35749883e62dca7e88f9165cdd1b215fca5
Christian Pojoni <dev@pojoni.at>
2019-11-16 02:44:39 +0100

    files moved

--- horoskop.rb
+++ horoskop.rb
@@ -7,41 +7,15 @@ require 'pp'
 require 'optimist'
 require 'yaml'
 require 'date'
-require_relative 'date_german_additions'
+require_relative 'lib/date_german_additions'
 
-class Horoscope
-  attr_reader :z_map
-  attr_accessor :data, :selected_zodiacs
-  @@ZODIACS = %w{aries taurus gemini cancer leo virgio libra scorpio sagittarius capricorn aquarius pisces}
-
-  def initialize(zodiacs)
-    @data = {}
-    @selected_zodiacs = zodiacs.join == "all" ? @@ZODIACS : zodiacs & @@ZODIACS
-    raise ArgumentError.new("zodiacs '#{zodiacs.join(', ')}' not found") if @selected_zodiacs.empty?
-  end
-
-  def download
-    raise "download needs to be implemented"
-  end
-
-  def contents
-    download if data.empty?
-    @data
-  rescue SocketError => e
-    $stderr.puts self.class.name << ": " << e.message
-  end
-
-  def zodiacs
-    @selected_zodiacs.map{ |z| @z_map[z] }.zip(@selected_zodiacs)
-  end
-end
-
-require_relative 'astrowoche'
-require_relative 'astroportal'
-require_relative 'kroneat'
-require_relative 'kurierat'
-require_relative 'horoscopecom'
-require_relative 'rogers'
+require_relative 'lib/horoscope'
+require_relative 'lib/astrowoche'
+require_relative 'lib/astroportal'
+require_relative 'lib/kroneat'
+require_relative 'lib/kurierat'
+require_relative 'lib/horoscopecom'
+require_relative 'lib/rogers'
 
 opts = Optimist::options do
   opt :zodiac, "Choose zodiac", :type => :string, :default => "all"       
---
+++ lib/horoscope.rb
@@ -0,0 +1,26 @@
+class Horoscope
+  attr_reader :z_map
+  attr_accessor :data, :selected_zodiacs
+  @@ZODIACS = %w{aries taurus gemini cancer leo virgio libra scorpio sagittarius capricorn aquarius pisces}
+
+  def initialize(zodiacs)
+    @data = {}
+    @selected_zodiacs = zodiacs.join == "all" ? @@ZODIACS : zodiacs & @@ZODIACS
+    raise ArgumentError.new("zodiacs '#{zodiacs.join(', ')}' not found") if @selected_zodiacs.empty?
+  end
+
+  def download
+    raise "download needs to be implemented"
+  end
+
+  def contents
+    download if data.empty?
+    @data
+  rescue SocketError => e
+    $stderr.puts self.class.name << ": " << e.message
+  end
+
+  def zodiacs
+    @selected_zodiacs.map{ |z| @z_map[z] }.zip(@selected_zodiacs)
+  end
+end
